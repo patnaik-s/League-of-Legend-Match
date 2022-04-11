450c51914864afcab24fc66a6b7877ea1281d748
Sneha Patnaik <46284119+patnaik-s@users.noreply.github.com>
2022-04-12 00:20:02 +0530

    Delete model.py

--- model.py
+++
@@ -1,8 +0,0 @@
-import tensorflow
-import numpy
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D, MaxPool2D
-from keras import backend as K
-import matplotlib.pyplot as plt
